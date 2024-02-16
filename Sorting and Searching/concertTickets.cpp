7a0cb8103985e80dd03f493644295f53cb39bdeb
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-02-16 15:13:14 +0600

    ..

--- Sorting and Searching/concertTickets.cpp
+++ Sorting and Searching/concertTickets.cpp
@@ -1,46 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*void checker(int tick[], int user[])
-{
-}
-*/
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    int tick[n], user[m];
+    int nu, m;
+    cin >> nu >> m;
+    vector<int> tick(nu), user(m);
+    for (auto &i : tick)
+        cin >> i;
+    for (auto &i : user)
+        cin >> i;
+    sort(tick.begin(), tick.end());
+    int n = min(tick.size(), user.size());
+
     for (int i = 0; i < n; i++)
     {
-        cin >> tick[i];
-    }
-
-    for (int i = 0; i < m; i++)
-    {
-        cin >> user[i];
-    }
-
-    sort(tick, tick + n);
-    //  checker(tick, user);
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
+        if (binary_search(tick.begin(), tick.end(), user[i]))
         {
-            if (user[i] >= tick[j])
-            {
-                if (user[i] == tick[j] || (user[i] < tick[j + 1] && user[i] > tick[j]))
-                {
-                    cout << tick[j] << endl;
-                    tick[j] = -1;
-                    break;
-                }
-                else if (user[i] < tick[0])
-                {
-                    cout << -1 << endl;
-                    break;
-                }
-            }
+            cout << user[i];
+            break;
+        }
+
+        if (user[i] < tick[0])
+        {
+            cout << -1 << endl;
+            break;
+        }
+
+        if (user[i] > tick[i] && user[i] < tick[i + 1])
+        {
+            cout << tick[i] << endl;
+            break;
         }
     }
+
+    /*  for (auto i : user)
+      {
+          if (binary_search(tick.begin(), tick.end(), user[i]))
+          {
+              cout << user[i];
+          }
+          else if ()
+          {
+          }
+      }
+      */
     return 0;
 }
